ec4e820d4e8ea4b5799f5fd4ff1697038ca266dd
Danilo <danilo.portelli@liparipeople.com>
2026-04-30 17:51:42 +0200

    fix css

--- src/core/pages/DashboardPage/Dashboard.tsx
+++ src/core/pages/DashboardPage/Dashboard.tsx
@@ -1,17 +1,28 @@
 import MovementRow from "../../../features/dashboard/components/MovementRow";
 import AccountSummaryCard from "../../../features/dashboard/components/AccountSummaryCard";
 import { accounts, movements } from "../../data";
+import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
-    <>
-      {accounts.map((account) => (
-        <AccountSummaryCard key={account.id} account={account} />
-      ))}
+    <div className={styles.container}>
+      {/* CONTI */}
+      <div className={styles.accountsSection}>
+        {accounts.map((account) => (
+          <AccountSummaryCard key={account.id} account={account} />
+        ))}
+      </div>
 
-      {movements.map((movement) => (
-        <MovementRow key={movement.id} movement={movement} />
-      ))}
-    </>
+      {/* MOVIMENTI */}
+      <div className={styles.movementsSection}>
+        <div className={styles.sectionHeader}>Movimenti recenti</div>
+
+        <div className={styles.movementsList}>
+          {movements.map((movement) => (
+            <MovementRow key={movement.id} movement={movement} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
